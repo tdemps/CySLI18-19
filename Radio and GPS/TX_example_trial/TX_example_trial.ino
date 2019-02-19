106706dad24999226f74b9adcb3777915b5d7105
Alex Harpenau <harpenau@iastate.edu>
2019-02-19 00:26:06 -0600

    added comments to the ground and rocket codes I added earlier.

--- Radio and GPS/TX_example_trial/TX_example_trial.ino
+++ Radio and GPS/TX_example_trial/TX_example_trial.ino
@@ -1,13 +1,3 @@
-// rf69 demo tx rx.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messageing client
-// with the RH_RF69 class. RH_RF69 class does not provide for addressing or
-// reliability, so you should only use RH_RF69  if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example rf69_server.
-// Demonstrates the use of AES encryption, setting the frequency and modem 
-// configuration
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
@@ -30,37 +20,34 @@ int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void setup() 
 {
+  // initializing serial and pins
   Serial.begin(115200);
-
   pinMode(LED, OUTPUT);     
   pinMode(RFM95_RST, OUTPUT);
+  // don't know why it needs to be set low to be set low again,
+  // but it works this way so no point in screwing it up,
+  // maybe if we have time
   digitalWrite(RFM95_RST, LOW);
-
-  Serial.println("Feather RFM69 TX Test!");
 
   // manual reset, need to keep so init doesn't fail
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
-  
+
+  // initializing the radio and frequency
   if (!rf95.init()) {
     Serial.println("RFM95 radio init failed");
     while (1);
-  }
-  Serial.println("RFM95 radio init OK!");
- 
+  } 
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
   }
 
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf95.setTxPower(23, true);  // range from 14-23 for power, 2nd arg must be true for 69HCW
+  // range from ?-23 for power
+  rf95.setTxPower(23, true);  
   
-  pinMode(LED, OUTPUT);
-
-  Serial.print("RFM95 radio @");  Serial.print((int)RF95_FREQ);  Serial.println(" MHz");
+  Serial.print("Transmitter Ready");
 }
 
 
@@ -68,28 +55,38 @@ void setup()
 void loop() {
   delay(1000);  // Wait 1 second between transmits, could also 'sleep' here!
   if (Serial.available()){
-  char input = Serial.read();
-  
+    // reads single char inputs from the serial monitor
+    char input = Serial.read();
+
+    // if it's 1, disengage the retention system
     if (input == '1'){
       char radiopacket[20] = "in";
+      
+      // pretty sure we don't need this line but I haven't tested it yet
       itoa(packetnum++, radiopacket+13, 10);
       Serial.print("Sending "); 
       Serial.println(radiopacket);  
       // Send a message!
       rf95.send((uint8_t *)radiopacket, strlen(radiopacket));
       rf95.waitPacketSent();
-      
+
+    // if it's 3, engage the retention system  
     } else if (input =='3'){
       char radiopacket[20] = "out";
+      
+      // pretty sure we don't need this line but I haven't tested it yet
       itoa(packetnum++, radiopacket+13, 10);
       Serial.print("Sending "); 
       Serial.println(radiopacket);
       // Send a message!
       rf95.send((uint8_t *)radiopacket, strlen(radiopacket));
       rf95.waitPacketSent();
-      
+
+    // if it's five, fire the ematch for the deployment system
     } else if (input == '5'){
       char radiopacket[20] = "fire";
+
+      // pretty sure we don't need this line but I haven't tested it yet
       itoa(packetnum++, radiopacket+13, 10);
       Serial.print("Sending "); 
       Serial.println(radiopacket);
@@ -99,6 +96,8 @@ void loop() {
       
     }
   }
+
+  // this is where GPS would be received, just need to write the other side.
   // Now wait for a reply
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
