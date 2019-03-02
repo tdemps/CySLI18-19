f557f75c11a67e1914575e3861c3e77b8fa6d8ba
Alex Harpenau <alexrharpenau@gmail.com>
2019-03-01 18:15:56 -0600

    I'm pretty sure I'm just adding to the libraries

---
+++ libraries/BNO055-BMP280/BNOBMPLib.h
@@ -0,0 +1,212 @@
+
+
+#ifndef BNOBMPLib_h	//DONT TOUCH
+#define BNOBMPLib_h	//ALSO DONT TOUCH
+
+#include <i2c_t3.h>
+#include <SPI.h>
+
+// BMP280 registers
+#define BMP280_TEMP_XLSB  0xFC
+#define BMP280_TEMP_LSB   0xFB
+#define BMP280_TEMP_MSB   0xFA
+#define BMP280_PRESS_XLSB 0xF9
+#define BMP280_PRESS_LSB  0xF8
+#define BMP280_PRESS_MSB  0xF7
+#define BMP280_CONFIG     0xF5
+#define BMP280_CTRL_MEAS  0xF4
+#define BMP280_STATUS     0xF3
+#define BMP280_RESET      0xE0
+#define BMP280_ID         0xD0  // should be 0x58
+#define BMP280_CALIB00    0x88
+
+// BNO055 Register Map
+// http://ae-bst.resource.bosch.com/media/products/dokumente/bno055/BST_BNO055_DS000_10_Release.pdf
+//
+// BNO055 Page 0
+#define BNO055_CHIP_ID          0x00    // should be 0xA0              
+#define BNO055_ACC_ID           0x01    // should be 0xFB              
+#define BNO055_MAG_ID           0x02    // should be 0x32              
+#define BNO055_GYRO_ID          0x03    // should be 0x0F              
+#define BNO055_SW_REV_ID_LSB    0x04                                                                          
+#define BNO055_SW_REV_ID_MSB    0x05
+#define BNO055_BL_REV_ID        0x06
+#define BNO055_PAGE_ID          0x07
+#define BNO055_ACC_DATA_X_LSB   0x08
+#define BNO055_ACC_DATA_X_MSB   0x09
+#define BNO055_ACC_DATA_Y_LSB   0x0A
+#define BNO055_ACC_DATA_Y_MSB   0x0B
+#define BNO055_ACC_DATA_Z_LSB   0x0C
+#define BNO055_ACC_DATA_Z_MSB   0x0D
+#define BNO055_MAG_DATA_X_LSB   0x0E
+#define BNO055_MAG_DATA_X_MSB   0x0F
+#define BNO055_MAG_DATA_Y_LSB   0x10
+#define BNO055_MAG_DATA_Y_MSB   0x11
+#define BNO055_MAG_DATA_Z_LSB   0x12
+#define BNO055_MAG_DATA_Z_MSB   0x13
+#define BNO055_GYR_DATA_X_LSB   0x14
+#define BNO055_GYR_DATA_X_MSB   0x15
+#define BNO055_GYR_DATA_Y_LSB   0x16
+#define BNO055_GYR_DATA_Y_MSB   0x17
+#define BNO055_GYR_DATA_Z_LSB   0x18
+#define BNO055_GYR_DATA_Z_MSB   0x19
+#define BNO055_EUL_HEADING_LSB  0x1A
+#define BNO055_EUL_HEADING_MSB  0x1B
+#define BNO055_EUL_ROLL_LSB     0x1C
+#define BNO055_EUL_ROLL_MSB     0x1D
+#define BNO055_EUL_PITCH_LSB    0x1E
+#define BNO055_EUL_PITCH_MSB    0x1F
+#define BNO055_QUA_DATA_W_LSB   0x20
+#define BNO055_QUA_DATA_W_MSB   0x21
+#define BNO055_QUA_DATA_X_LSB   0x22
+#define BNO055_QUA_DATA_X_MSB   0x23
+#define BNO055_QUA_DATA_Y_LSB   0x24
+#define BNO055_QUA_DATA_Y_MSB   0x25
+#define BNO055_QUA_DATA_Z_LSB   0x26
+#define BNO055_QUA_DATA_Z_MSB   0x27
+#define BNO055_LIA_DATA_X_LSB   0x28
+#define BNO055_LIA_DATA_X_MSB   0x29
+#define BNO055_LIA_DATA_Y_LSB   0x2A
+#define BNO055_LIA_DATA_Y_MSB   0x2B
+#define BNO055_LIA_DATA_Z_LSB   0x2C
+#define BNO055_LIA_DATA_Z_MSB   0x2D
+#define BNO055_GRV_DATA_X_LSB   0x2E
+#define BNO055_GRV_DATA_X_MSB   0x2F
+#define BNO055_GRV_DATA_Y_LSB   0x30
+#define BNO055_GRV_DATA_Y_MSB   0x31
+#define BNO055_GRV_DATA_Z_LSB   0x32
+#define BNO055_GRV_DATA_Z_MSB   0x33
+#define BNO055_TEMP             0x34
+#define BNO055_CALIB_STAT       0x35
+#define BNO055_ST_RESULT        0x36
+#define BNO055_INT_STATUS       0x37
+#define BNO055_SYS_CLK_STATUS   0x38
+#define BNO055_SYS_STATUS       0x39
+#define BNO055_SYS_ERR          0x3A
+#define BNO055_UNIT_SEL         0x3B
+#define BNO055_OPR_MODE         0x3D
+#define BNO055_PWR_MODE         0x3E
+#define BNO055_SYS_TRIGGER      0x3F
+#define BNO055_TEMP_SOURCE      0x40
+#define BNO055_AXIS_MAP_CONFIG  0x41
+#define BNO055_AXIS_MAP_SIGN    0x42
+#define BNO055_ACC_OFFSET_X_LSB 0x55
+#define BNO055_ACC_OFFSET_X_MSB 0x56
+#define BNO055_ACC_OFFSET_Y_LSB 0x57
+#define BNO055_ACC_OFFSET_Y_MSB 0x58
+#define BNO055_ACC_OFFSET_Z_LSB 0x59
+#define BNO055_ACC_OFFSET_Z_MSB 0x5A
+#define BNO055_MAG_OFFSET_X_LSB 0x5B
+#define BNO055_MAG_OFFSET_X_MSB 0x5C
+#define BNO055_MAG_OFFSET_Y_LSB 0x5D
+#define BNO055_MAG_OFFSET_Y_MSB 0x5E
+#define BNO055_MAG_OFFSET_Z_LSB 0x5F
+#define BNO055_MAG_OFFSET_Z_MSB 0x60
+#define BNO055_GYR_OFFSET_X_LSB 0x61
+#define BNO055_GYR_OFFSET_X_MSB 0x62
+#define BNO055_GYR_OFFSET_Y_LSB 0x63
+#define BNO055_GYR_OFFSET_Y_MSB 0x64
+#define BNO055_GYR_OFFSET_Z_LSB 0x65
+#define BNO055_GYR_OFFSET_Z_MSB 0x66
+#define BNO055_ACC_RADIUS_LSB   0x67
+#define BNO055_ACC_RADIUS_MSB   0x68
+#define BNO055_MAG_RADIUS_LSB   0x69
+#define BNO055_MAG_RADIUS_MSB   0x6A
+//
+// BNO055 Page 1
+#define BNO055_PAGE_ID          0x07
+#define BNO055_ACC_CONFIG       0x08
+#define BNO055_MAG_CONFIG       0x09
+#define BNO055_GYRO_CONFIG_0    0x0A
+#define BNO055_GYRO_CONFIG_1    0x0B
+#define BNO055_ACC_SLEEP_CONFIG 0x0C
+#define BNO055_GYR_SLEEP_CONFIG 0x0D
+#define BNO055_INT_MSK          0x0F
+#define BNO055_INT_EN           0x10
+#define BNO055_ACC_AM_THRES     0x11
+#define BNO055_ACC_INT_SETTINGS 0x12
+#define BNO055_ACC_HG_DURATION  0x13
+#define BNO055_ACC_HG_THRESH    0x14
+#define BNO055_ACC_NM_THRESH    0x15
+#define BNO055_ACC_NM_SET       0x16
+#define BNO055_GYR_INT_SETTINGS 0x17
+#define BNO055_GYR_HR_X_SET     0x18
+#define BNO055_GYR_DUR_X        0x19
+#define BNO055_GYR_HR_Y_SET     0x1A
+#define BNO055_GYR_DUR_Y        0x1B
+#define BNO055_GYR_HR_Z_SET     0x1C
+#define BNO055_GYR_DUR_Z        0x1D
+#define BNO055_GYR_AM_THRESH    0x1E
+#define BNO055_GYR_AM_SET       0x1F
+
+
+// Using the BNO055_BMP280 breakout board/Teensy 3.1 Add-On Shield, ADO is set to 1 by default 
+#define ADO 1
+#if ADO
+#define BNO055_ADDRESS 0x29   //  Device address of BNO055 when ADO = 1
+#define BMP280_ADDRESS 0x77   // Address of BMP280
+#else
+#define BNO055_ADDRESS 0x28   //  Device address of BNO055 when ADO = 0
+#define BMP280_ADDRESS 0x77   // Address of BMP280
+#endif  
+
+#define SerialDebug true      // set to true to get Serial output for debugging
+
+
+void DebugSetup();
+
+void notloop();
+
+//===================================================================================================================
+//====== Set of useful function to access acceleration. gyroscope, magnetometer, and temperature data
+//===================================================================================================================
+
+void readAccelData(int16_t * arr);
+
+void readGyroData(int16_t * destination);
+
+int8_t readGyroTempData();
+
+void readMagData(int16_t * destination);
+
+void readQuatData(int16_t * destination);
+
+void readEulData(int16_t * destination);
+
+void readLIAData(int16_t * destination);
+
+void readGRVData(int16_t * destination);
+
+void initBNO055();
+
+void accelgyroCalBNO055(float * dest1, float * dest2);
+
+void magCalBNO055(float * dest1);
+
+
+int32_t readBMP280Temperature();
+
+int32_t readBMP280Pressure();
+
+void BMP280Init();
+
+// Returns temperature in DegC, resolution is 0.01 DegC. Output value of
+// “5123” equals 51.23 DegC.
+int32_t bmp280_compensate_T(int32_t adc_T);
+
+// Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24 integer bits and 8
+//fractional bits).
+//Output value of “24674867” represents 24674867/256 = 96386.2 Pa = 963.862 hPa
+uint32_t bmp280_compensate_P(int32_t adc_P);
+
+// I2C scan function
+
+void I2Cscan();
+        
+void writeByte(uint8_t address, uint8_t subAddress, uint8_t data);
+
+uint8_t readByte(uint8_t address, uint8_t subAddress);
+
+void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest);
+double BMPAltitude(float seaLevel);
+#endif
